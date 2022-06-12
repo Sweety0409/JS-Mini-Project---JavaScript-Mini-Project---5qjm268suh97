5b583897d86db757cf45315b037b143e939835e6
Sweety Rani <104487537+Sweety0409@users.noreply.github.com>
2022-06-12 22:35:52 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,1 +1,20 @@
+function validateUser()
+{
+var username=document.getElementById("username").value;
+var password=document.getElementById("password").value;
+if((username=="sweety"&& password=="rani")||(username=="admin"&& password=="newton"))
+{
+    alert("login succesfully");
+    
+    document.getElementById("loginSucessful").innerHTML="login succesfully"
+}
+else if(username==""&&password==""){
+    alert("field can't be empty");
+}
+else
+{
+    alert("login failed. The username or password is incorrect");
+    document.getElementById("loginFail").innerHTML="login failed"
 
+}
+}
